baa27c4e76ca00fbe3d531a32cc1bfb53e2d6d2d
lucasgabriel95 <lucas.gabriel.95@gmail.com>
2020-06-15 20:13:30 -0300

    Tela de Registro Incerida

--- AutomaoCasa/app/src/main/java/com/example/automaocasa/LoginActivity.kt
+++ AutomaoCasa/app/src/main/java/com/example/automaocasa/LoginActivity.kt
@@ -3,7 +3,7 @@ package com.example.automaocasa
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AutomationActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
